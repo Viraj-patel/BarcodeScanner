59e862022a3def42834afecaa17b4a257e597525
Viraj patel <viraj@toddleapp.com>
2021-10-03 03:24:35 +0530

    added dashboard routes

--- src/Dashboard/Dashboard.js
+++ src/Dashboard/Dashboard.js
@@ -1,29 +1,80 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Dashboard.css";
 import logo from "../nonsenselogo.jpg";
+import { Redirect } from "react-router";
 
 const Dashboard = () => {
-  return (
+  const [redirect, setRedirect] = useState(false);
+  const [redirectTo, setRedirectTo] = useState("");
+  return redirect ? (
+    <Redirect to={{ pathname: `/${redirectTo}` }} />
+  ) : (
     <div className="container">
       <img src={logo} height="30px" alt="Nonsense" />
       <div></div>
       <div>
-        <button className="buttonClass">Add new Product</button>
+        <button
+          className="buttonClass"
+          onClick={() => {
+            setRedirect(true);
+            setRedirectTo("addProduct");
+          }}
+        >
+          Add new Product
+        </button>
       </div>
       <div>
-        <button className="buttonClass">Add product in Stock</button>
+        <button
+          className="buttonClass"
+          onClick={() => {
+            setRedirect(true);
+            setRedirectTo("add");
+          }}
+        >
+          Add product in Stock
+        </button>
       </div>
       <div>
-        <button className="buttonClass">Remove Product from stock</button>
+        <button
+          className="buttonClass"
+          onClick={() => {
+            setRedirect(true);
+            setRedirectTo("remove");
+          }}
+        >
+          Remove Product from stock
+        </button>
       </div>
       <div>
-        <button className="buttonClass">Add Stock Manually</button>
+        <button
+          className="buttonClass"
+          onClick={() => {
+            setRedirect(true);
+            setRedirectTo("addmanually");
+          }}
+        >
+          Add Stock Manually
+        </button>
       </div>
       <div>
-        <button className="buttonClass">Remove Stock Manually</button>
+        <button
+          className="buttonClass"
+          onClick={() => {
+            setRedirect(true);
+            setRedirectTo("removeManually");
+          }}
+        >
+          Remove Stock Manually
+        </button>
       </div>
       <div>
-        <button className="buttonClass">
+        <button
+          className="buttonClass"
+          onClick={() => {
+            setRedirect(true);
+            setRedirectTo("list");
+          }}
+        >
           Stock details and product Barcodes
         </button>
       </div>
