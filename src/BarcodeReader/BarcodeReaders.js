3b98626fabda2d41a5ff8376c49ef631f3a1fb61
Viraj patel <viraj@toddleapp.com>
2021-11-17 02:57:57 +0530

    import changes

--- src/BarcodeReader/BarcodeReaders.js
+++ src/BarcodeReader/BarcodeReaders.js
@@ -1,86 +1,9 @@
-// import React, { Component } from "react";
-// import Scanner from "./Scanner";
-import logo from "../nonsenselogo.jpg";
-
-// import BarcodeReader from "react-barcode-reader";
-// class BarcodeReaders extends Component {
-//   state = {
-//     scanning: false,
-//     redirect: false,
-//     redirectTo: "",
-//     results: [],
-//     data: [],
-//   };
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       result: "No result",
-//     };
-
-//     this.handleScan = this.handleScan.bind(this);
-//   }
-//   componentDidMount() {
-//     this.loadBrcode();
-//   }
-
-//   loadBrcode = () => {
-//     var response = fetch("https://non-sense-backend.herokuapp.com/getProduct")
-//       .then((response) => {
-//         return response.json();
-//       })
-//       .then((myJson) => {
-//         this.setState({ data: myJson });
-//       });
-//   };
-
-//   // _scan = () => {
-//   //   this.setState({ scanning: !this.state.scanning });
-//   // };
-
-//   _onDetected = (scanResult) => {
-//     console.log(scanResult, _.get(scanResult, "codeResult.code", ""));
-//     this.setState({ scanning: !this.state.scanning });
-//     this.setState({
-//       results: this.state.results.concat([scanResult]),
-//     });
-//   };
-//   handleScan(data) {
-//     this.setState({
-//       result: data,
-//     });
-//   }
-//   handleError(err) {
-//     console.error(err);
-//   }
-//   render() {
-//     console.log(this.state.results);
-//     return this.state.redirect ? (
-//       <Redirect to={{ pathname: `../` }} />
-//     ) : (
-//       <div className="container">
-//         <img
-//           src={logo}
-//           height="30px"
-//           alt="Nonsense"
-//           onClick={() => {
-//             this.setState({ redirect: true });
-//           }}
-//         />
-//         <button onClick={this._scan}>Add new</button>
-
-//         {/* <Result results={this.state.results} data={this.state.data} /> */}
-//         <BarcodeReader onError={this.handleError} onScan={this.handleScan} />
-//         <p>{this.state.result}</p>
-//         {/* {this.state.scanning ? <Scanner onDetected={this._onDetected} /> : null} */}
-//       </div>
-//     );
-//   }
-// }
-
 import React, { useState, useEffect } from "react";
 import BarcodeScannerComponent from "react-qr-barcode-scanner";
 import Result from "./Result";
 import { Redirect } from "react-router";
+import logo from "../nonsenselogo.jpg";
+
 var _ = require("lodash");
 
 const BarcodeReaders = () => {
